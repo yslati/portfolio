a89a74e55052b5b4b3c259ab63d60fcc4a4bedb2
yslati <kaliyassin01@gmail.com>
2024-03-08 12:27:47 +0100

    fix image display and text in phone

--- src/Display.tsx
+++ src/Display.tsx
@@ -1,8 +1,8 @@
 export default function Display() {
   return (
-    <div className="w-1/2 h-mmax pb-10">
-        <div className="rounded-4xl border border-gray-800 min-h-full w-full">
-            <img src="/luffy.jpg" alt="smoke" className="rounded-4xl object-fill h-full w-full" />
+    <div className="w-1/2 h-full pb-10 max-h-full lg:flex flex-col hidden">
+        <div className="rounded-4xl border border-gray-800 w-full h-full">
+            <img src="/luffy.jpg" alt="luffy" className="rounded-4xl object-center h-full w-full" />
         </div>
         <p className="text-white text-center text-xs font-mono font-light mt-4">
             Mt. Tamalpais 37.9236° N, 122.5965° W
